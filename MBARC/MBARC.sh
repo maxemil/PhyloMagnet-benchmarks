2612e80b89b2be7695795de1ddfda42b14b7faa1
maxemil <max-emil.schon@icm.uu.se>
2018-12-07 12:23:49 +0100

    fix paths in MBARC script

--- MBARC/MBARC.sh
+++ MBARC/MBARC.sh
@@ -1,8 +1,32 @@
-nextflow run /local/two/Software/PhyloMagnet/main.nf \
-            --lineage "Clostridium","Ruminiclostridium","Coraliomargarita","Corynebacterium","Desulfosporosinus","Desulfotomaculum","Echinicola","Escherichia","Fervidobacterium","Frateuria","Halovivax","Hirschia","Meiothermus","Natronobacterium","Natronococcus","Nocardiopsis","Olsenella","Pseudomonas","Salmonella","Segniliparus","Sediminispirochaeta","Streptococcus","Terriglobus","Thermobacillus","Enterobacteriaceae" \
-            -with-singularity  /local/two/Software/PhyloMagnet/PhyloMagnet.simg \
-            -resume \
-            --cpus 40 \
+/usr/bin/time --output=queries_1perc_timed.txt -v nextflow run maxemil/PhyloMagnet \
+            --lineage "genus" \
+            -with-singularity  ../PhyloMagnet.simg \
+            --cpus 10 \
             --reference_packages "../rp16_added_rpkg/*.tgz*" \
-            --fastq "fastq/*" \
-            --megan_vmoptions "../MEGAN.vmoptions"
+            --fastq "fastq/SRR3656745.1perc.fastq.gz" \
+            --megan_vmoptions "../MEGAN.vmoptions" \
+            -with-report MBARC_report_1perc.html \
+            --queries_dir queries_1perc
+
+/usr/bin/time --output=queries_10perc_timed.txt -v nextflow run maxemil/PhyloMagnet \
+            --lineage "genus" \
+            -with-singularity  ../PhyloMagnet.simg \
+            --cpus 10 \
+            --reference_packages "../rp16_added_rpkg/*.tgz*" \
+            --fastq "fastq/SRR3656745.10perc.fastq.gz" \
+            --megan_vmoptions "../MEGAN.vmoptions" \
+            -with-report MBARC_report_10perc.html \
+            --queries_dir queries_10perc
+
+/usr/bin/time --output=queries_timed.txt -v nextflow run maxemil/PhyloMagnet \
+            --lineage "genus" \
+            -with-singularity  ../PhyloMagnet.simg \
+            --cpus 10 \
+            --reference_packages "../rp16_added_rpkg/*.tgz*" \
+            --fastq "fastq/SRR3656745.fastq.gz" \
+            --megan_vmoptions "../MEGAN.vmoptions" \
+            -with-report MBARC_report.html \
+            --queries_dir queries
+
+
+            # --lineage "Clostridium","Ruminiclostridium","Coraliomargarita","Corynebacterium","Desulfosporosinus","Desulfotomaculum","Echinicola","Escherichia","Fervidobacterium","Frateuria","Halovivax","Hirschia","Meiothermus","Natronobacterium","Natronococcus","Nocardiopsis","Olsenella","Pseudomonas","Salmonella","Segniliparus","Sediminispirochaeta","Streptococcus","Terriglobus","Thermobacillus","Enterobacteriaceae" \
